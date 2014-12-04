523edb17d10a178910bd7d29ed0de1d4aca7d9c8
Jeff Reifman <jeff@newscloud.com>
2014-12-03 16:37:04 -0800

    Updated

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -2,13 +2,13 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-$this->title = 'About';
+$this->title = 'About - Meeting Planner';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
+    <p>Meeting Planner is a new app designed to make scheduling as simple as it should be. However, Meeting Planner will be developed as part of a <a href="http://code.tutsplus.com">Tuts+ Code tutorial series</a> by <a href="http://tutsplus.com/authors/jeff-reifman">author Jeff Reifman</a>. Every piece of Meeting Planner will be built out in the open as the series progresses. The application is being developed in the <a href="https://code.tutsplus.com/tutorials/programming-with-yii2-getting-started--cms-22440">Yii2 Framework</a> for PHP.</p>
+    <p>You can learn more about Jeff or contact him at his <a href="http://jeffreifman.com">website</a>. You can also follow him on Twitter <a href="https://twitter.com/intent/user?screen_name=reifman">@reifman</a>.</p>
 
-    <code><?= __FILE__ ?></code>
 </div>
